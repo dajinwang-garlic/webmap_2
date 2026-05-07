397b9887afb8cd61cc37f5cb00f314f0dc319428
dajin-garlic <dw3091@columbia.edu>
2026-05-07 16:00:25 -0400

    update map

---
+++ map.js
@@ -0,0 +1,14 @@
+console.log("Hello, world!")
+'use strict'        // let the browser know we're serious
+
+// debug statement letting us know the file is loaded
+console.log('Loaded map.js')
+
+// your mapbox token
+mapboxgl.accessToken = 'YOUR TOKEN HERE BETWEEN THE QUOTES'
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v11',
+    center: [-73.93324, 40.80877],
+    zoom: 14
+});
